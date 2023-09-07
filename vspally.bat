dc1b06dab9046d7391d2891471ebfbac4d44a442
Sridhar Bandari <sridhar.bandari@fujitsu.com>
2023-09-07 13:29:48 +0100

    try

---
+++ vspally.bat
@@ -0,0 +1,1 @@
+pa11y-ci  --config .\vspally.pallyci  -j  > results_10_8_2023_10_27.json
---
+++ vspallyr.bat
@@ -0,0 +1,1 @@
+pa11y-ci-reporter-html  -s  .\results_10_8_2023_10_27.json -d .\results_html_10_8_2023_10_27
